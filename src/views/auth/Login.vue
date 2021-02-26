8f1660067f2d53297288d6ee2a82281275c287dc
Aleksandar <90stepeni@gmail.com>
2021-02-26 18:46:05 +0100

    login and registration validation

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -3,12 +3,16 @@
 		<form @submit.prevent="handleLogin">
 			<div class="form-group">
 				<label class="form-check-label" for="email">Enter your email</label>
-				<input v-model="formData.email" type="email" class="form-control" id="email" />
+				<input v-model="formData.email" type="email" class="form-control" id="email" :class="validation || 'is-invalid'" />
 			</div>
 			<div class="form-group">
 				<label class="form-check-label" for="password">Enter password</label>
-				<input v-model="formData.password" type="password" class="form-control" id="password" />
+				<input v-model="formData.password" type="password" class="form-control" id="password" :class="validation || 'is-invalid'" />
+				<div class="invalid-feedback">
+					Wrong email or password
+				</div>
 			</div>
+
 			<button type="submit" class="btn btn-primary">Login</button>
 		</form>
 	</div>
@@ -24,16 +28,18 @@ export default {
 				email: '',
 				password: '',
 			},
+			validation: true,
 		};
 	},
 	methods: {
 		...mapActions('auth', ['login']),
 		async handleLogin() {
 			try {
+				this.validation = true;
 				await this.login(this.formData);
 				this.$router.push('/');
 			} catch (error) {
-				console.log(error);
+				this.validation = false;
 			}
 		},
 	},
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -2,28 +2,37 @@
 	<div class="container">
 		<form @submit.prevent="handleRegister">
 			<div class="form-group">
-				<label class="form-check-label" for="name">First name</label>
-				<input v-model="formData.first_name" type="text" class="form-control" id="first_name" />
+				<label class="form-check-label is-valid" for="name">First name</label>
+				<input required v-model="formData.first_name" type="text" class="form-control" id="first_name" />
 			</div>
 			<div class="form-group">
 				<label class="form-check-label" for="name">Last name</label>
-				<input v-model="formData.last_name" type="text" class="form-control" id="last_name" />
+				<input required v-model="formData.last_name" type="text" class="form-control" id="last_name" />
 			</div>
 			<div class="form-group">
 				<label class="form-check-label" for="email">Email address</label>
-				<input v-model="formData.email" type="email" class="form-control" id="email" />
+				<input required v-model="formData.email" type="email" class="form-control" id="email" />
 			</div>
 			<div class="form-group">
 				<label class="form-check-label" for="password">Set password</label>
-				<input v-model="formData.password" type="password" class="form-control" id="password" />
+				<input v-model="formData.password" type="password" class="form-control" :class="validation.password || 'is-invalid'" id="password" />
+				<div class="invalid-feedback">
+					Pasword must have at least 8 characters and 1 need to be a number.
+				</div>
 			</div>
 			<div class="form-group">
 				<label class="form-check-label" for="confirm_password">Confirm password</label>
-				<input v-model="formData.confirm_password" type="password" class="form-control" id="confirm_password" />
+				<input v-model="formData.confirm_password" type="password" class="form-control" :class="validation.confirm_password || 'is-invalid'" id="confirm_password" />
+				<div class="invalid-feedback">
+					Confirm password must match password.
+				</div>
 			</div>
 			<div class="form-check">
-				<input v-model="formData.terms" type="checkbox" class="form-check-input" id="terms" />
+				<input v-model="formData.terms" type="checkbox" class="form-check-input" :class="validation.terms || 'is-invalid'" id="terms" />
 				<label class="form-check-label" for="exampleCheck1">I accept terms and conditions</label>
+				<div class="invalid-feedback">
+					To register you must accept terms and conditions.
+				</div>
 			</div>
 			<button type="submit" class="btn btn-primary">Register</button>
 		</form>
@@ -44,12 +53,35 @@ export default {
 				confirm_password: '',
 				terms: false,
 			},
+			validation: {
+				password: true,
+				confirm_password: true,
+				terms: true,
+			},
 		};
 	},
 	methods: {
 		...mapActions('auth', ['register']),
+		validate() {
+			let errors = 0;
+			const passwordRegex = /^(?=.*\d).+$/;
+			if (this.formData.password.length < 8 || !passwordRegex.test(this.formData.password)) {
+				this.validation.password = false;
+				errors++;
+			} else this.validation.password = true;
+			if (!(this.formData.password === this.formData.confirm_password)) {
+				this.validation.confirm_password = false;
+				errors++;
+			} else this.validation.confirm_password = true;
+			if (!this.formData.terms) {
+				this.validation.terms = false;
+				errors++;
+			} else this.validation.terms = true;
+			return !errors;
+		},
 		async handleRegister() {
 			try {
+				if (!this.validate()) return;
 				await this.register(this.formData);
 				this.$router.push('/');
 			} catch (error) {
